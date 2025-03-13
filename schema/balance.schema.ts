125a848e7700379142f9b1f062d2f065c4e020c1
0xKits <kitsgit@gmail.com>
2025-03-13 12:54:24 +0530

    added balance schema

---
+++ schema/balance.schema.ts
@@ -0,0 +1,11 @@
+import * as p from "drizzle-orm/pg-core";
+import { users } from "./users.schema";
+const E_BALANCE_LOG_TYPE = p.pgEnum("E_BALANCE_LOG_TYPE", ["DEPOSIT", "WITHDRAWAL", "BET", "WIN"])
+export const balance_log = p.pgTable("balance_log", {
+    id: p.uuid("id").primaryKey(),
+    account: p.uuid("account").references(() => users.id),
+    amount: p.numeric("amount").notNull(),
+    type: E_BALANCE_LOG_TYPE().notNull(),
+    created_at: p.timestamp("created_at").notNull().defaultNow(),
+});
+
