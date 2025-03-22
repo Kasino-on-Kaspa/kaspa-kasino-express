61e60a00cb057b90e7956d3968b2e92b7f7e5dfc
Vishal <vismbs@pm.me>
2025-03-22 21:31:25 +0530

    refactor: message changes

--- services/games/dieroll/dieroll.messages.ts
+++ services/games/dieroll/dieroll.messages.ts
@@ -1,11 +1,11 @@
 // Client -> Server messages
 export const enum DieRollClientMessage {
-    GET_SESSION_SEEDS = 'dieroll:get_session_seeds',
-    PLACE_BET = 'dieroll:place_bet'
+  GET_SESSION = "dieroll:get_session",
+  PLACE_BET = "dieroll:place_bet",
 }
 
 // Server -> Client messages
 export const enum DieRollServerMessage {
-    ROLL_RESULT = 'dieroll:roll_result',
-    GAME_ENDED = 'dieroll:game_ended'
+  ROLL_RESULT = "dieroll:roll_result",
+  GAME_ENDED = "dieroll:game_ended",
 }
