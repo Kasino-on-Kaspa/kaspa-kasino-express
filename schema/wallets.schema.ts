665b3c5a19d2b3a46af39272f2d54e56370bdde5
0xKits <kitsgit@gmail.com>
2025-03-22 19:13:52 +0530

    minor schema change to wallet

--- schema/wallets.schema.ts
+++ schema/wallets.schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import * as p from "drizzle-orm/pg-core";
 
 // Wallet table to track deposit addresses
@@ -14,6 +15,7 @@ export const wallets = p.pgTable("wallets", {
 	// xOnly Public Key
 	xOnlyPublicKey: p.varchar().notNull(),
 
+	balance: p.bigint({ mode: "bigint" }).notNull().default(sql`0`),
 	// Timestamps
 	createdAt: p.timestamp().notNull().defaultNow(),
 });
