45f65153d42f1cdd9966f815c9264b5e890a3d57
0x0bi <vyshnavpnair2005@gmail.com>
2025-04-18 18:56:59 +0530

    feat(referral): added logic to referral payout

--- services/user/referral/referral.controller.ts
+++ services/user/referral/referral.controller.ts
@@ -9,23 +9,33 @@ import { EventBus } from "@utils/eventbus";
 
 export class ReferralController {
   private RefferalModel: ReferralModel = new ReferralModel();
+
+  private WIN_REFERRAL_PAYOUT_PERCENTAGE = 20 * 100 * 100;
+  private LOSE_REFERRAL_PAYOUT_PERCENTAGE = 10 * 100 * 100;
+  
   public async handleGameCompleted(data: {
     account: { username: string; id: string };
     result: "WIN" | "LOSE" | "DRAW";
     bet: number;
     payout: number;
   }) {
-    if (data.result != "WIN") return;
+    if (data.result == "DRAW") return;
     let account = AccountStoreInstance.GetUserFromAccountID(data.account.id);
     let refferal: string | null = null;
+    
     if (account) refferal = account.Referral;
     else refferal = await this.RefferalModel.GetRefferalAccountID(data.account.id);
-
-    if (!refferal) return;
+    
+    if (!refferal) return;    
     let refferalWallet = await this.RefferalModel.GetRefferalWalletID(refferal);
+    let payout:number;
+
+    if (data.result == "WIN") payout = data.payout * this.WIN_REFERRAL_PAYOUT_PERCENTAGE/(100*100);
+    else payout = Math.abs(data.payout) * this.LOSE_REFERRAL_PAYOUT_PERCENTAGE / (100*100);
+    
     EventBus.Instance.emit("wallet:update", {
       walletID: refferalWallet,
-      balance: data.payout * 0.05,
+      balance: payout,
       type: "REFFEAL_RETURN",
     });
   }
